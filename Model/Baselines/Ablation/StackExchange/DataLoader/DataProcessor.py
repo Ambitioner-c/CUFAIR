3a7c2ce388d50bf3cf4e659bc86e679c35d6fefb
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-09-05 09:12:03 +0800

    change cprint as coloring

--- Model/Baselines/Ablation/StackExchange/DataLoader/DataProcessor.py
+++ Model/Baselines/Ablation/StackExchange/DataLoader/DataProcessor.py
@@ -2,7 +2,6 @@
 # @Author: Fulai Cui (cuifulai@mail.hfut.edu.cn)
 # @Time: 2024/8/23 11:17
 import os.path
-import random
 import re
 from abc import ABC
 
@@ -12,35 +11,7 @@ from transformers import DataProcessor
 
 import xml.etree.ElementTree as ET
 
-
-def cprint(content: str, color: str=None) -> str:
-    if color == 'None':
-        return content
-
-    colors = {
-        'red': '\033[31m',
-        'green': '\033[32m',
-        'yellow': '\033[33m',
-        'blue': '\033[34m',
-        'purple': '\033[35m',
-        'cyan': '\033[36m',
-        'white': '\033[37m',
-        'red_bg': '\033[41m',
-        'green_bg': '\033[42m',
-        'yellow_bg': '\033[43m',
-        'blue_bg': '\033[44m',
-        'purple_bg': '\033[45m',
-        'cyan_bg': '\033[46m',
-        'white_bg': '\033[47m',
-    }
-    if color is None:
-        while True:
-            color = random.choice(list(colors.keys()))
-            if color.endswith('_bg'):
-                continue
-            else:
-                break
-    return f'{colors[color]}{content}\033[0m'
+from Model.Unit.cprint import *
 
 
 class PingMatch:
@@ -168,9 +139,9 @@ class PingMatch:
         if save:
             print(f"{'-' * 25}Answer ID: {a_id}{'-' * 25}", file=f)
         if show:
-            print(f"{cprint('Poster ', 'red_bg')}", participants[0])
+            print(f"{coloring('Poster ', 'red_bg')}", participants[0])
         if save:
-            print(f"{cprint('Poster ', 'None')}", participants[0], file=f)
+            print(f"{coloring('Poster ', 'None')}", participants[0], file=f)
 
         symbols = [' _____ ' for _ in range(length)]
         links = []
@@ -189,33 +160,33 @@ class PingMatch:
             return _symbols
         for link in sorted(links, reverse=True):
             if show:
-                print('       ', cprint(''.join(connection(link))))
+                print('       ', coloring(''.join(connection(link))))
             if save:
-                print('       ', cprint(''.join(connection(link)), 'None'), file=f)
+                print('       ', coloring(''.join(connection(link)), 'None'), file=f)
         if show:
             print('       ', ''.join(symbols))
         if save:
             print('       ', ''.join(symbols), file=f)
 
         if show:
-            print(cprint('Name   ', 'green_bg'), ''.join([f'  {x[:3]}. ' for x in participants[1:]]))
+            print(coloring('Name   ', 'green_bg'), ''.join([f'  {x[:3]}. ' for x in participants[1:]]))
         if save:
-            print(cprint('Name   ', 'None'), ''.join([f'  {x[:3]}. ' for x in participants[1:]]), file=f)
+            print(coloring('Name   ', 'None'), ''.join([f'  {x[:3]}. ' for x in participants[1:]]), file=f)
 
         if show:
-            print(cprint('Index  ', 'yellow_bg'), ''.join([f"{'    ' + str(index + 1) + '  '}"[-7:] for index in range(length)]))
+            print(coloring('Index  ', 'yellow_bg'), ''.join([f"{'    ' + str(index + 1) + '  '}"[-7:] for index in range(length)]))
         if save:
-            print(cprint('Index  ', 'None'), ''.join([f"{'    ' + str(index + 1) + '  '}"[-7:] for index in range(length)]), file=f)
+            print(coloring('Index  ', 'None'), ''.join([f"{'    ' + str(index + 1) + '  '}"[-7:] for index in range(length)]), file=f)
 
         if show:
-            print(cprint('Ping   ', 'blue_bg'), ''.join([f"{'    ' + str(ping) + '  '}"[-7:] for ping in pings]))
+            print(coloring('Ping   ', 'blue_bg'), ''.join([f"{'    ' + str(ping) + '  '}"[-7:] for ping in pings]))
         if save:
-            print(cprint('Ping   ', 'None'), ''.join([f"{'    ' + str(ping) + '  '}"[-7:] for ping in pings]), file=f)
+            print(coloring('Ping   ', 'None'), ''.join([f"{'    ' + str(ping) + '  '}"[-7:] for ping in pings]), file=f)
 
         if show:
-            print(cprint('Score  ', 'purple_bg'), ''.join([f"{'    ' + str(score) + '  '}"[-7:] for score in scores]))
+            print(coloring('Score  ', 'purple_bg'), ''.join([f"{'    ' + str(score) + '  '}"[-7:] for score in scores]))
         if save:
-            print(cprint('Score  ', 'None'), ''.join([f"{'    ' + str(score) + '  '}"[-7:] for score in scores]), file=f)
+            print(coloring('Score  ', 'None'), ''.join([f"{'    ' + str(score) + '  '}"[-7:] for score in scores]), file=f)
 
         if save:
             f.close()
@@ -259,7 +230,7 @@ class SEProcessor(DataProcessor, ABC):
         lefts = []
         rights = []
         labels = []
-        for elem in tqdm(self.iterparse(filepath), desc=f'Parsing {cprint(filepath, "red")} XML file'):
+        for elem in tqdm(self.iterparse(filepath), desc=f'Parsing {coloring(filepath, "red")} XML file'):
             # Answer
             for answer in elem.findall('Answer'):
                 if int(answer.attrib['COMMENT_COUNT']):
