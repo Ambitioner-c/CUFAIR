421ab1eb04915de17b7de65ece2d2d3ef5714e74
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-10-04 16:13:18 +0800

    add save_args_to_file

--- Model/Unit/function.py
+++ Model/Unit/function.py
@@ -1,12 +1,20 @@
 # coding=utf-8
 # @Author: Fulai Cui (cuifulai@mail.hfut.edu.cn)
 # @Time: 2024/9/5 20:35
+import json
 import os
 
 
 def mkdir(file_dir: str) -> str:
     os.makedirs(os.path.dirname(file_dir), exist_ok=True)
     return file_dir
+
+
+def save_args_to_file(args, file_path: str):
+    args_dict = vars(args)
+
+    with open(file_path, 'w') as f:
+        json.dump(args_dict, f, indent=4)
 
 
 def main():
