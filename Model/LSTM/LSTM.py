16cb3fd4562f88f1690701848b033ada784cb06f
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-10-08 19:10:07 +0800

    change name

--- Model/LSTM/LSTM.py
+++ Model/LSTM/LSTM.py
@@ -104,11 +104,11 @@ class LSTMModel(nn.Module):
         super(LSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.vanilla_lstm = LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x: Tensor) -> Tensor:
-        out, _ = self.vanilla_lstm(x, None)
+        out, _ = self.lstm(x, None)
         out = self.fc(out)
 
         return out
