33ac4da522606df17c53fb4a4ebf2d998fa99c61
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-10-14 08:53:22 +0800

    cuda:0

--- Model/Baselines/Ablation/OM_wo_CI/OM_wo_CI.py
+++ Model/Baselines/Ablation/OM_wo_CI/OM_wo_CI.py
@@ -318,7 +318,7 @@ def parse_args():
                         help='Data directory')
     parser.add_argument('--data_name', nargs='?', default='meta.stackoverflow.com',
                         help='Data name')
-    parser.add_argument('--device', nargs='?', default='cuda:1',
+    parser.add_argument('--device', nargs='?', default='cuda:0',
                         help='Device')
     parser.add_argument('--dropout_prob', type=float, default=0.1,
                         help='Dropout probability')
