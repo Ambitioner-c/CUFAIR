d45026bab3e4677f9bde4372a9bc43989aea85dd
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-10-19 11:16:47 +0800

    BiLSTM

--- Model/LSTM/BiLSTM.py
+++ Model/LSTM/BiLSTM.py
@@ -44,7 +44,7 @@ class LSTMCell(nn.Module):
         return h, c
 
 
-class LSTM(nn.Module):
+class BiLSTM(nn.Module):
     def __init__(
             self,
             input_size: int,
@@ -52,14 +52,17 @@ class LSTM(nn.Module):
             num_layers: int = 1,
             batch_first: bool = False,
     ):
-        super(LSTM, self).__init__()
+        super(BiLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.batch_first = batch_first
-        layers = [LSTMCell(input_size, hidden_size)]
-        for _ in range(self.num_layers - 1):
-            layers += [LSTMCell(hidden_size, hidden_size)]
-        self.net = nn.Sequential(*layers)
+        self.forward_net = nn.Sequential(*[LSTMCell(input_size if i == 0 else hidden_size, hidden_size) for i in range(num_layers)])
+        self.backward_net = nn.Sequential(*[LSTMCell(input_size if i == 0 else hidden_size, hidden_size) for i in range(num_layers)])
+
+        self.h_forward = None
+        self.c_forward = None
+        self.h_backward = None
+        self.c_backward = None
 
         self.h = None
         self.c = None
@@ -74,18 +77,29 @@ class LSTM(nn.Module):
         if self.batch_first:
             x = x.transpose(0, 1)
 
-        self.h = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
-        self.c = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
+        self.h_forward = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
+        self.c_forward = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
+        self.h_backward = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
+        self.c_backward = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
         if init_states is not None:
             self.h[0], self.c[0] = init_states
 
         inputs = x
-        for i, cell in enumerate(self.net):  # Layers
-            h_t, c_t = self.h[0, i].clone(), self.c[0, i].clone()
+        for i, (forward_cell, backward_cell) in enumerate(zip(self.forward_net, self.backward_net)):  # Layers
+            # Forward
+            h_t, c_t = self.h_forward[0, i].clone(), self.c_forward[0, i].clone()
             for t in range(x.size(0)):  # Sequences
-                h_t, c_t = cell(inputs[t], (h_t, c_t))
-                self.h[t, i], self.c[t, i] = h_t, c_t
-            inputs = self.h[:, i].clone()
+                h_t, c_t = forward_cell(inputs[t], (h_t, c_t))
+                self.h_forward[t, i], self.c_forward[t, i] = h_t, c_t
+            # Backward
+            h_t, c_t = self.h_backward[0, i].clone(), self.c_backward[0, i].clone()
+            for t in range(x.size(0)):  # Sequences
+                h_t, c_t = backward_cell(inputs[t], (h_t, c_t))
+                self.h_backward[t, i], self.c_backward[t, i] = h_t, c_t
+            inputs = torch.cat((self.h_forward[:, i], self.h_backward[:, i]), dim=-1).clone()
+
+        self.h = torch.cat((self.h_forward, self.h_backward), dim=-1)
+        self.c = torch.cat((self.c_forward, self.c_backward), dim=-1)
 
         if self.batch_first:
             return self.h[:, -1].transpose(0, 1), (self.h[-1], self.c[-1])
@@ -93,7 +107,7 @@ class LSTM(nn.Module):
         return self.h[:, -1], (self.h[-1], self.c[-1])
 
 
-class LSTMModel(nn.Module):
+class BiLSTMModel(nn.Module):
     def __init__(
             self,
             input_size: int,
@@ -101,14 +115,14 @@ class LSTMModel(nn.Module):
             num_layers: int,
             output_size: int
     ):
-        super(LSTMModel, self).__init__()
+        super(BiLSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.bilstm = BiLSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size * 2, output_size)
 
     def forward(self, x: Tensor) -> Tensor:
-        out, _ = self.lstm(x, None)
+        out, _ = self.bilstm(x, None)
         out = self.fc(out)
 
         return out
@@ -122,10 +136,10 @@ def main():
     input_size = 10
     inputs = torch.randn(batch_size, seq_length, input_size)
 
-    hidden_size = 5
-    num_layers = 2
-    output_size = 5
-    model = LSTMModel(input_size, hidden_size, num_layers, output_size)
+    hidden_size = 10
+    num_layers = 1
+    output_size = 10
+    model = BiLSTMModel(input_size, hidden_size, num_layers, output_size)
 
     outputs = model(inputs)
     print(outputs.size())
