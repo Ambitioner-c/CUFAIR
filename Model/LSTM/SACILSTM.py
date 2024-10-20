60bebbba32aab420bb0d4684532ccf92a47ba621
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-10-20 10:53:41 +0800

    change attention as answer

--- Model/LSTM/SACILSTM.py
+++ Model/LSTM/SACILSTM.py
@@ -15,16 +15,16 @@ from Model.Unit.modeling_bert import BertSelfAttention
 class SACILSTMCell(nn.Module):
     def __init__(
             self,
-            attention_size: int,
+            answer_size: int,
             input_size: int,
             hidden_size: int,
             is_peephole: bool = False,
     ):
         super(SACILSTMCell, self).__init__()
-        self.attention_size = attention_size
+        self.answer_size = answer_size
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.weight_ah = nn.Parameter(Tensor(attention_size, hidden_size * 4))
+        self.weight_ah = nn.Parameter(Tensor(answer_size, hidden_size * 4))
         self.weight_ih = nn.Parameter(Tensor(input_size, hidden_size * 4))
         self.weight_hh = nn.Parameter(Tensor(hidden_size, hidden_size * 4))
         self.bias_ah = nn.Parameter(Tensor(hidden_size * 4))
@@ -41,16 +41,16 @@ class SACILSTMCell(nn.Module):
 
     def forward(
             self,
-            attention: Tensor,
+            answer: Tensor,
             x: Tensor,
             init_states: Tuple[Tensor, Tensor]
     ) -> Tuple[Tensor, Tensor]:
         h_t_minus_1, c_t_minus_1 = init_states
 
-        temp = torch.mm(attention, self.weight_ah) + self.bias_ah
+        a_temp = torch.mm(answer, self.weight_ah) + self.bias_ah
         if self.is_peephole:
-            temp[:, self.hidden_size * 2: self.hidden_size * 3] = 0
-        gates = temp + torch.mm(x, self.weight_ih) + self.bias_ih + torch.mm(h_t_minus_1, self.weight_hh) + self.bias_hh
+            a_temp[:, self.hidden_size * 2: self.hidden_size * 3] = 0
+        gates = a_temp + torch.mm(x, self.weight_ih) + self.bias_ih + torch.mm(h_t_minus_1, self.weight_hh) + self.bias_hh
         input_gate, forget_gate, cell, output_gate = gates.chunk(4, dim=1)
         c = torch.sigmoid(forget_gate) * c_t_minus_1 + torch.sigmoid(input_gate) * torch.tanh(cell)
         h = torch.sigmoid(output_gate) * torch.tanh(c)
@@ -61,7 +61,7 @@ class SACILSTMCell(nn.Module):
 class SACILSTM(nn.Module):
     def __init__(
             self,
-            attention_size: int,
+            answer_size: int,
             input_size: int,
             hidden_size: int,
             num_layers: int = 1,
@@ -74,9 +74,9 @@ class SACILSTM(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.batch_first = batch_first
-        layers = [SACILSTMCell(attention_size, input_size, hidden_size, is_peephole)]
+        layers = [SACILSTMCell(answer_size, input_size, hidden_size, is_peephole)]
         for _ in range(self.num_layers - 1):
-            layers += [SACILSTMCell(attention_size, hidden_size, hidden_size, is_peephole)]
+            layers += [SACILSTMCell(answer_size, hidden_size, hidden_size, is_peephole)]
         self.net = nn.Sequential(*layers)
 
         self.h = None
@@ -90,7 +90,7 @@ class SACILSTM(nn.Module):
 
     def forward(
             self,
-            attention: Tensor,
+            answer: Tensor,
             x: Tensor,
             ping: Tensor,
             init_states: Tuple[Tensor, Tensor]
@@ -98,7 +98,7 @@ class SACILSTM(nn.Module):
         # Input and output size: (batch_size, seq_length, input_size)
         # States size: (num_layers, batch_size, hidden_size)
         if self.batch_first:
-            attention = attention.transpose(0, 1)
+            answer = answer.transpose(0, 1)
             x = x.transpose(0, 1)
 
         self.h = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
@@ -112,7 +112,7 @@ class SACILSTM(nn.Module):
             for t in range(x.size(0)):  # Sequences
                 inputs = self.refactoring(inputs, ping, t).clone()
 
-                h_t, c_t = cell(attention[0], inputs[t], (h_t, c_t))
+                h_t, c_t = cell(answer[0], inputs[t], (h_t, c_t))
                 self.h[t, i], self.c[t, i] = h_t, c_t
             inputs = self.h[:, i].clone()
 
@@ -154,7 +154,7 @@ class SACILSTM(nn.Module):
 class SACILSTMModel(nn.Module):
     def __init__(
             self,
-            attention_size: int,
+            answer_size: int,
             input_size: int,
             hidden_size: int,
             num_layers: int,
@@ -166,11 +166,11 @@ class SACILSTMModel(nn.Module):
         super(SACILSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.sacilstm = SACILSTM(attention_size, input_size, hidden_size, num_layers, num_attention_heads, batch_first=True, is_peephole=is_peephole, ci_mode=ci_mode)
+        self.sacilstm = SACILSTM(answer_size, input_size, hidden_size, num_layers, num_attention_heads, batch_first=True, is_peephole=is_peephole, ci_mode=ci_mode)
         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, attention: Tensor, x: Tensor, ping: Tensor) -> Tensor:
-        out, _ = self.sacilstm(attention, x, ping, None)
+    def forward(self, answer: Tensor, x: Tensor, ping: Tensor) -> Tensor:
+        out, _ = self.sacilstm(answer, x, ping, None)
         out = self.fc(out)
 
         return out
@@ -182,7 +182,7 @@ def main():
     batch_size = 5
     seq_length = 3
     input_size = 12
-    attentions = torch.randn(batch_size, 1, input_size)
+    answers = torch.randn(batch_size, 1, input_size)
     inputs = torch.randn(batch_size, seq_length, input_size)
     pings = torch.tensor(
         [[0, 0, 0],
@@ -199,7 +199,7 @@ def main():
     is_peephole = False
     ci_mode = 'all'
     model = SACILSTMModel(input_size, input_size, hidden_size, num_layers, output_size, num_attention_heads, is_peephole, ci_mode)
-    outputs = model(attentions, inputs, pings)
+    outputs = model(answers, inputs, pings)
     print(outputs.size())
 
 
