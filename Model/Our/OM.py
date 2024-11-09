9967114ff25566f3ba44e0f23a933037708bcab4
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-11-09 13:24:11 +0800

    add mae loss

--- Model/Our/OM.py
+++ Model/Our/OM.py
@@ -292,10 +292,12 @@ def eval_ranking_metrics_on_data_frame(
 
 
 def evaluate(args, task_name, model, test_dataloader, timestamp, save_test):
-    support_loss_function = nn.MSELoss()
+    mse_loss_function = nn.MSELoss()
+    mae_loss_function = nn.L1Loss()
 
     predictions = []
-    support_losses = []
+    mse_losses = []
+    mae_losses = []
     for test_sample in test_dataloader:
         test_inputs, _, supports = test_sample
         mask = supports >= 0
@@ -303,10 +305,13 @@ def evaluate(args, task_name, model, test_dataloader, timestamp, save_test):
         with torch.no_grad():
             test_outputs, test_output_supports = model(test_inputs)
 
-            test_support_loss = support_loss_function(input=test_output_supports[mask], target=supports[mask].to(model.device))
+            mse_loss = mse_loss_function(input=test_output_supports[mask], target=supports[mask].to(model.device))
+            mae_loss = mae_loss_function(input=test_output_supports[mask], target=supports[mask].to(model.device))
 
-            if not torch.isnan(test_support_loss):
-                support_losses.append(test_support_loss.item())
+            if not torch.isnan(mse_loss):
+                mse_losses.append(mse_loss.item())
+            if not torch.isnan(mae_loss):
+                mae_losses.append(mae_loss.item())
 
             predictions.append(test_outputs.detach().cpu())
     y_pred = torch.cat(predictions, dim=0).numpy()
@@ -316,7 +321,8 @@ def evaluate(args, task_name, model, test_dataloader, timestamp, save_test):
         eval_ranking_metrics_on_data_frame(id_left, y_true, y_pred.squeeze(axis=-1)))
     best_test_result = (
         f'{task_name}\t'
-        f'{coloring("support_loss", "purple_bg")}:{np.mean(support_losses)}\t'
+        f'{coloring("mse_loss", "purple_bg")}:{np.mean(mse_losses)}\t'
+        f'{coloring("mae_loss", "purple_bg")}:{np.mean(mae_losses)}\t'
         f'Ranking:\t'
         f'{coloring("p@1", "red_bg")}:{round(p_1, 4)}\t'
         f'{coloring("p@3", "red_bg")}:{round(p_3, 4)}\t'
