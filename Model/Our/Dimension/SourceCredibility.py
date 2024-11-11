9717c28e18df0090a1a5d6acbe6b3e6e8a044248
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-11-11 08:48:44 +0800

    SourceCredibility

---
+++ Model/Our/Dimension/SourceCredibility.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+# @Author: Fulai Cui (cuifulai@mail.hfut.edu.cn)
+# @Time: 2024/11/10 22:05
+class SourceCredibility:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_features():
+        pass
+
+    @staticmethod
+    def get_features_name():
+        pass
+
+    def get_credibility(self):
+        pass
+
+    def get_past_experience(self):
+        pass
+
+    def get_trustworthiness(self):
+        pass
+
+    def get_expertise(self):
+        pass
+
+
+def simulation():
+    pass
+
+
+def fact():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
