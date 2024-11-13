51ea847113eeb7c8db9aa2bc026a0042666116d6
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-11-13 11:13:26 +0800

    copy from SQACILSTM

--- Model4AfD/LSTM/SNCILSTM.py
+++ Model4AfD/LSTM/SNCILSTM.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # @Author: Fulai Cui (cuifulai@mail.hfut.edu.cn)
-# @Time: 2024/10/8 19:47
+# @Time: 2024/10/8 16:49
 from typing import Tuple
 
 import torch
@@ -12,20 +12,31 @@ from transformers import set_seed
 from Model.Unit.modeling_bert import BertSelfAttention
 
 
-class CILSTMCell(nn.Module):
+class SQACILSTMCell(nn.Module):
     def __init__(
             self,
+            question_size: int,
+            answer_size: int,
             input_size: int,
-            hidden_size: int
+            hidden_size: int,
+            is_peephole: bool = False,
     ):
-        super(CILSTMCell, self).__init__()
+        super(SQACILSTMCell, self).__init__()
+        self.question_size = question_size
+        self.answer_size = answer_size
         self.input_size = input_size
         self.hidden_size = hidden_size
+        self.weight_qh = nn.Parameter(Tensor(question_size, hidden_size * 4))
+        self.weight_ah = nn.Parameter(Tensor(answer_size, hidden_size * 4))
         self.weight_ih = nn.Parameter(Tensor(input_size, hidden_size * 4))
         self.weight_hh = nn.Parameter(Tensor(hidden_size, hidden_size * 4))
+        self.bias_qh = nn.Parameter(Tensor(hidden_size * 4))
+        self.bias_ah = nn.Parameter(Tensor(hidden_size * 4))
         self.bias_ih = nn.Parameter(Tensor(hidden_size * 4))
         self.bias_hh = nn.Parameter(Tensor(hidden_size * 4))
         self.init_parameters()
+
+        self.is_peephole = is_peephole
 
     def init_parameters(self):
         stdv = 1.0 / math.sqrt(self.hidden_size)
@@ -34,11 +45,19 @@ class CILSTMCell(nn.Module):
 
     def forward(
             self,
+            question: Tensor,
+            answer: Tensor,
             x: Tensor,
             init_states: Tuple[Tensor, Tensor]
     ) -> Tuple[Tensor, Tensor]:
         h_t_minus_1, c_t_minus_1 = init_states
-        gates = torch.mm(x, self.weight_ih) + self.bias_ih + torch.mm(h_t_minus_1, self.weight_hh) + self.bias_hh
+
+        q_temp = torch.mm(question, self.weight_qh) + self.bias_qh
+        a_temp = torch.mm(answer, self.weight_ah) + self.bias_ah
+        if self.is_peephole:
+            q_temp[:, self.hidden_size * 2: self.hidden_size * 3] = 0
+            a_temp[:, self.hidden_size * 2: self.hidden_size * 3] = 0
+        gates = q_temp + a_temp + torch.mm(x, self.weight_ih) + self.bias_ih + torch.mm(h_t_minus_1, self.weight_hh) + self.bias_hh
         input_gate, forget_gate, cell, output_gate = gates.chunk(4, dim=1)
         c = torch.sigmoid(forget_gate) * c_t_minus_1 + torch.sigmoid(input_gate) * torch.tanh(cell)
         h = torch.sigmoid(output_gate) * torch.tanh(c)
@@ -46,23 +65,26 @@ class CILSTMCell(nn.Module):
         return h, c
 
 
-class CILSTM(nn.Module):
+class SQACILSTM(nn.Module):
     def __init__(
             self,
+            question_size: int,
+            answer_size: int,
             input_size: int,
             hidden_size: int,
             num_layers: int = 1,
             num_attention_heads = 12,
             batch_first: bool = False,
+            is_peephole: bool = False,
             ci_mode: str = 'all',
     ):
-        super(CILSTM, self).__init__()
+        super(SQACILSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.batch_first = batch_first
-        layers = [CILSTMCell(input_size, hidden_size)]
+        layers = [SQACILSTMCell(question_size, answer_size, input_size, hidden_size, is_peephole)]
         for _ in range(self.num_layers - 1):
-            layers += [CILSTMCell(hidden_size, hidden_size)]
+            layers += [SQACILSTMCell(question_size, answer_size, hidden_size, hidden_size, is_peephole)]
         self.net = nn.Sequential(*layers)
 
         self.h = None
@@ -76,6 +98,8 @@ class CILSTM(nn.Module):
 
     def forward(
             self,
+            question: Tensor,
+            answer: Tensor,
             x: Tensor,
             ping: Tensor,
             init_states: Tuple[Tensor, Tensor]
@@ -83,6 +107,8 @@ class CILSTM(nn.Module):
         # Input and output size: (batch_size, seq_length, input_size)
         # States size: (num_layers, batch_size, hidden_size)
         if self.batch_first:
+            question = question.transpose(0, 1)
+            answer = answer.transpose(0, 1)
             x = x.transpose(0, 1)
 
         self.h = torch.zeros(x.size(0), self.num_layers, x.size(1), self.hidden_size).to(x.device)
@@ -96,7 +122,7 @@ class CILSTM(nn.Module):
             for t in range(x.size(0)):  # Sequences
                 inputs = self.refactoring(inputs, ping, t).clone()
 
-                h_t, c_t = cell(inputs[t], (h_t, c_t))
+                h_t, c_t = cell(question[0], answer[0], inputs[t], (h_t, c_t))
                 self.h[t, i], self.c[t, i] = h_t, c_t
             inputs = self.h[:, i].clone()
 
@@ -135,24 +161,27 @@ class CILSTM(nn.Module):
         return new_inputs
 
 
-class CILSTMModel(nn.Module):
+class SQACILSTMModel(nn.Module):
     def __init__(
             self,
+            question_size: int,
+            answer_size: int,
             input_size: int,
             hidden_size: int,
             num_layers: int,
             output_size: int,
             num_attention_heads: int,
+            is_peephole: bool,
             ci_mode: str,
     ):
-        super(CILSTMModel, self).__init__()
+        super(SQACILSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.cilstm = CILSTM(input_size, hidden_size, num_layers, num_attention_heads, batch_first=True, ci_mode=ci_mode)
+        self.sqacilstm = SQACILSTM(question_size, answer_size, input_size, hidden_size, num_layers, num_attention_heads, batch_first=True, is_peephole=is_peephole, ci_mode=ci_mode)
         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x: Tensor, ping: Tensor) -> Tensor:
-        out, _ = self.cilstm(x, ping, None)
+    def forward(self, question: Tensor, answer: Tensor, x: Tensor, ping: Tensor) -> Tensor:
+        out, _ = self.sqacilstm(question, answer, x, ping, None)
         out = self.fc(out)
 
         return out
@@ -164,6 +193,8 @@ def main():
     batch_size = 5
     seq_length = 3
     input_size = 12
+    questions = torch.randn(batch_size, 1, input_size)
+    answers = torch.randn(batch_size, 1, input_size)
     inputs = torch.randn(batch_size, seq_length, input_size)
     pings = torch.tensor(
         [[0, 0, 0],
@@ -177,10 +208,10 @@ def main():
     num_layers = 2
     output_size = 5
     num_attention_heads = 12
+    is_peephole = False
     ci_mode = 'all'
-    model = CILSTMModel(input_size, hidden_size, num_layers, output_size, num_attention_heads, ci_mode)
-
-    outputs = model(inputs, pings)
+    model = SQACILSTMModel(input_size, input_size, input_size, hidden_size, num_layers, output_size, num_attention_heads, is_peephole, ci_mode)
+    outputs = model(questions, answers, inputs, pings)
     print(outputs.size())
 
 
