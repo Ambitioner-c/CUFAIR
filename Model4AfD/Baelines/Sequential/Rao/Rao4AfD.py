2dcf325489487ee2fb0754125ea7d99cf996b203
Cui <58166925+Ambitioner-c@users.noreply.github.com>
2024-11-14 09:43:51 +0800

    Rao4AfD

--- Model4AfD/Baelines/Sequential/Rao/Rao4AfD.py
+++ Model4AfD/Baelines/Sequential/Rao/Rao4AfD.py
@@ -254,7 +254,7 @@ def parse_args():
     parser.add_argument('--task_name', nargs='?', default='Rao4AfD',
                         help='Task name')
 
-    parser.add_argument('--batch_size', type=int, default=100,
+    parser.add_argument('--batch_size', type=int, default=20,
                         help='Batch size')
     parser.add_argument('--bert_hidden_size', type=int, default=768,
                         help='Bert hidden size')
@@ -264,7 +264,7 @@ def parse_args():
                         help='Data directory')
     parser.add_argument('--data_name', nargs='?', default='AfD',
                         help='Data name')
-    parser.add_argument('--device', nargs='?', default='cuda:3',
+    parser.add_argument('--device', nargs='?', default='cuda:2',
                         help='Device')
     parser.add_argument('--dropout_prob', type=float, default=0.1,
                         help='Dropout probability')
@@ -272,7 +272,7 @@ def parse_args():
                         help='Number of epochs')
     parser.add_argument('--finetuned_model_path', nargs='?', default='./FinetunedModel/XXX/best_model.pth',
                         help='Finetuned model path')
-    parser.add_argument('--freeze', type=bool, default=True,
+    parser.add_argument('--freeze', type=bool, default=False,
                         help='Freeze')
     parser.add_argument('--hidden_size', type=int, default=108,
                         help='Hidden size')
@@ -300,7 +300,7 @@ def parse_args():
                         help='Random seed')
     parser.add_argument('--seq_length', type=int, default=10,
                         help='Max sequence length')
-    parser.add_argument('--step', type=int, default=1,
+    parser.add_argument('--step', type=int, default=100,
                         help='Step')
 
     return parser.parse_args()
